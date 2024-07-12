53d46f049576e59bd58e727caffb88fda93b49fb
DOMESTIC\hasnain.abbas1 <hasnain.abbas1@hbl.com>
2024-07-12 17:38:40 +0500

    lecture-5  code done

--- app/src/main/java/com/example/e_commerce_selfapp/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce_selfapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.e_commerce_selfapp
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
 import android.os.Bundle
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,18 +9,22 @@ import com.example.e_commerce_selfapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     private val adapter = ProductCardListAdapter()
+
+    private val viewModel : ProductListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.viewProductList.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.viewProductList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.viewProductList.adapter = adapter
-        updateUI(ProductListViewState.Content((1..3).map {
-            ProductCardViewState("Playstation $it","This is a nice console! Check it out","200 US$")
-        }))
-//        updateUI(ProductListViewState.Error("Currently showing error")) // comment to check git push
+
+        viewModel.viewState.observe(this){viewState ->
+            updateUI(viewState)
+        }
+        viewModel.loadProductList()
+
     }
 
     private fun updateUI(viewState: ProductListViewState) {
@@ -34,8 +39,6 @@ class MainActivity : AppCompatActivity() {
                 binding.loadingView.isVisible = false
                 binding.viewProductList.isVisible = false
                 binding.errorView.isVisible = false
-                binding.errorText.isVisible = true
-                binding.errorText.text = viewState.errorMsg
             }
             ProductListViewState.Loading -> {
                 binding.viewProductList.isVisible = false
--- app/src/main/java/com/example/e_commerce_selfapp/ProductListViewState.kt
+++ app/src/main/java/com/example/e_commerce_selfapp/ProductListViewState.kt
@@ -3,6 +3,6 @@ package com.example.e_commerce_selfapp
 sealed class ProductListViewState {
     object Loading : ProductListViewState()
     data class Content(val productList : List<ProductCardViewState>) : ProductListViewState()
-    data class Error(val errorMsg : String) : ProductListViewState()
+    object Error : ProductListViewState()
 
 }
